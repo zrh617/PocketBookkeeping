5735cdcae41b4c8cd528291fc4022a65576af50b
zrh617 <1298248084@qq.com>
2020-11-25 16:29:04 +0800

    导航栏样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,66 @@
 import React from 'react';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+import styled from 'styled-components';
+import Nav from './components/Nav';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`
 
 function App() {
+  // @ts-ignore
   return (
-    <div>Hello</div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Redirect exact from="/" to="/bookkeeping"/>
+            <Route path="/tags">
+              <Tags />
+            </Route>
+            <Route path="/bookkeeping">
+              <Bookkeeping />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            <Route path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
+        </Main>
+        <Nav/>
+      </Wrapper>
+    </Router>
   );
 }
 
+function NoMatch(){
+  return(
+    <h2>页面不存在</h2>
+  )
+}
+
+function Statistics() {
+  return <h2>统计页面</h2>;
+}
+
+function Tags() {
+  return <h2>标签页面</h2>;
+}
+
+function Bookkeeping() {
+  return <h2>记账页面</h2>;
+}
 export default App;
