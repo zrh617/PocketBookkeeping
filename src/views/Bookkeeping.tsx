ee052c7d0369df10e080bd75cf3b9a7a5590b1dd
zrh617 <1298248084@qq.com>
2020-11-29 15:18:25 +0800

    记账备注UI完成

--- src/views/Bookkeeping.tsx
+++ src/views/Bookkeeping.tsx
@@ -24,16 +24,30 @@ const TagsSection = styled.section`
     color: #666;
     margin-top: 8px;
   }
-`
+`;
 const NotesSection = styled.section`
-
-`
+  background: #f5f5f5;
+  padding: 0 16px;
+  font-size: 14px;
+  >label{
+    display: flex;
+    align-items: center;
+    >span{ margin-right: 16px;white-space: nowrap; }
+    >input{
+      display: block; 
+      width: 100%;
+      height: 72px;
+      background: none;
+      border: none;
+    }
+  }
+`;
 const CategorySection = styled.section`
 
-`
+`;
 const NumberPadSection = styled.section`
 
-`
+`;
 
 function Bookkeeping() {
   return (
@@ -50,14 +64,14 @@ function Bookkeeping() {
       <NotesSection>
         <label>
           <span>备注</span>
-          <input type="text"/>
+          <input type="text" placeholder="在这里添加备注"/>
         </label>
       </NotesSection>
       <CategorySection>
-         <ul>
-           <li>支出</li>
-           <li>收入</li>
-         </ul>
+        <ul>
+          <li>支出</li>
+          <li>收入</li>
+        </ul>
       </CategorySection>
       <NumberPadSection>
         <div>100</div>
