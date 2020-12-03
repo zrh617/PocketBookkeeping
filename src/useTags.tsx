f67b1473058ad7d0e6e21f790c9c653555c72708
zrh617 <1298248084@qq.com>
2020-12-03 14:26:32 +0800

    编辑不标签名

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -10,10 +10,29 @@ const defaultTags = [
 const useTags = () => {
   const [tags, setTags] = useState<{ id: number, name: string }[]>(defaultTags);
   const findTag = (id: number) => tags.filter(tag => tag.id === id)[0];
+  const findTagIndex = (id: number) => {
+    let result = -1;
+    for (let i = 0; i < tags.length; i++) {
+      if (tags[i].id === id) {
+        result = i;
+        break;
+      }
+    }
+    return result;
+  };
+  const updateTag = (id: number, obj: { name: string }) => {
+    const index = findTagIndex(id);
+    //深拷贝 tags 得到 tagsClone
+    const tagsClone = JSON.parse(JSON.stringify(tags));
+    tagsClone.splice(index, 1, {id: 1, name: obj.name});
+    setTags(tagsClone);
+  };
   return {
     tags,
     setTags,
-    findTag
+    findTag,
+    findTagIndex,
+    updateTag
   };
 };
 
