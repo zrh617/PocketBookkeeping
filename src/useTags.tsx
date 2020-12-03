0df46133b3401ef9f2fcc40c982293ef6fe895e8
zrh617 <1298248084@qq.com>
2020-12-03 14:51:04 +0800

    删除标签

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -21,18 +21,30 @@ const useTags = () => {
     return result;
   };
   const updateTag = (id: number, obj: { name: string }) => {
+    //获取需要修改的标签的下标
     const index = findTagIndex(id);
     //深拷贝 tags 得到 tagsClone
     const tagsClone = JSON.parse(JSON.stringify(tags));
+    //把tagsClone的第index项删除，替换为{id: id, name: obj.name}
     tagsClone.splice(index, 1, {id: 1, name: obj.name});
     setTags(tagsClone);
   };
+  const deleteTag = (id:number) => {
+    //获取需要删除的标签的下标
+    const index = findTagIndex(id);
+    //深拷贝 tags 得到 tagsClone
+    const tagsClone = JSON.parse(JSON.stringify(tags));
+    //把tagsClone的第index项删除}
+    tagsClone.splice(index, 1);
+    setTags(tagsClone);
+  }
   return {
     tags,
     setTags,
     findTag,
     findTagIndex,
-    updateTag
+    updateTag,
+    deleteTag
   };
 };
 
