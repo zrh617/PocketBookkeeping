5386aeefcf2c943e22ffa00775e9352daf20f2d7
zrh617 <1298248084@qq.com>
2020-12-03 10:39:16 +0800

    创建单独的Input组件

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -4,15 +4,15 @@ let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireCo
 try {importAll(require.context('icons', true, /\.svg$/));} catch (error) {console.log(error);}
 
 type Props = {
-  name: string
+  name?: string
 }
 
 const Icon = (props: Props) => {
-  return(
+  return (
     <svg className="icon">
-      <use xlinkHref={'#' + props.name}/>
+      {props.name && <use xlinkHref={'#' + props.name}/>}
     </svg>
-  )
-}
+  );
+};
 
-export default Icon
+export default Icon;
--- src/views/Bookkeeping/NotesSection.tsx
+++ src/views/Bookkeeping/NotesSection.tsx
@@ -1,22 +1,11 @@
 import styled from 'styled-components';
-import React, {useRef} from 'react';
+import React, {ChangeEventHandler, useRef} from 'react';
+import {Input} from 'components/Input';
 
 const Notes = styled.section`
   background: #f5f5f5;
   padding: 0 16px;
   font-size: 14px;
-  >label{
-    display: flex;
-    align-items: center;
-    >span{ margin-right: 16px;white-space: nowrap; }
-    >input{
-      display: block; 
-      width: 100%;
-      height: 72px;
-      background: none;
-      border: none;
-    }
-  }
 `;
 
 type Props = {
@@ -25,19 +14,12 @@ type Props = {
 }
 const NotesSection: React.FC<Props> = (props) => {
   const note = props.value;
-  const refInput = useRef<HTMLInputElement>(null);
-  const onBlur = () => {
-    if(refInput.current !== null){
-      props.onChange(refInput.current.value)
-    }
-  }
+  const onChange:ChangeEventHandler<HTMLInputElement> = (e) => {
+    props.onChange(e.target.value);
+  };
   return (
     <Notes>
-      <label>
-        <span>备注</span>
-        <input type="text" placeholder="在这里添加备注" ref={refInput}
-               defaultValue={note} onBlur={onBlur}/>
-      </label>
+      <Input label="备注" type="text" value={note} onChange={onChange} />
     </Notes>
   );
 };
